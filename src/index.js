1845d6aaf858613c5e1b4aa86745aaf9730c09a2
rise42 <oleksandr.pohrebniak42@gmail.com>
2018-12-29 21:39:39 +0200

    Access-Control headers added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  next()
+})
 
 app.get('/', (request, response) => response.send('Server here'))
 app.use('/login', loginRoute)
