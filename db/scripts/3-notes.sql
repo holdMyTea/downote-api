11b4fcbd3bc0e79719ade31d63bead4ecfc7c966
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2019-11-20 03:22:56 -0500

    Finalize procedures for notes table

--- db/scripts/3-notes.sql
+++ db/scripts/3-notes.sql
@@ -16,20 +16,39 @@ CREATE PROCEDURE downote.insert_note (
   IN order_value INT(8) UNSIGNED,
   IN user_value INT(8) UNSIGNED
 ) BEGIN
-  INSERT INTO downote.notes(note_header, note_text, note_order, user_id) VALUES (header_value, text_value, order_value, user_value);
+  INSERT INTO downote.notes(note_header, note_text, note_order, user_id)
+    VALUES (header_value, text_value, order_value, user_value);
 END$$
 
-CREATE PROCEDURE downote.update_note (
+CREATE PROCEDURE downote.update_note_content (
   IN id_value INT(40) UNSIGNED,
   IN header_value VARCHAR(255),
-  IN text_value TEXT,
+  IN text_value TEXT
+) BEGIN
+  UPDATE downote.notes SET
+    note_header = header_value,
+    note_text = text_value,
+    updated_time = CURRENT_TIMESTAMP
+    WHERE id = id_value;
+END$$
+
+CREATE PROCEDURE downote.update_note_order (
+  IN id_value INT(40) UNSIGNED,
   IN order_value INT(8) UNSIGNED
 ) BEGIN
-  UPDATE downote.notes SET note_header = header_value, note_text = text_value, note_order = order_value, updated_time = CURRENT_TIMESTAMP WHERE id = id_value;
+  UPDATE downote.notes SET
+    note_order = order_value
+    WHERE id = id_value;
 END$$
 
 CREATE PROCEDURE downote.remove_note (
   IN id_value INT(40) UNSIGNED
 ) BEGIN
   DELETE FROM downote.notes WHERE id = id_value;
+END$$
+
+CREATE PROCEDURE downote.get_user_notes (
+  IN user_value INT(8) UNSIGNED
+) BEGIN
+  SELECT * FROM downote.notes WHERE user_id = user_value;
 END$$
